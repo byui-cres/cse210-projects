4f9c2295cc9683faa9ee5839c38310df5300c92a
byui-cres <pab22006@byui.edu>
2023-01-04 09:36:19 +0800

    Learning Activity - C# Prep 1 - DONE!

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Console.WriteLine("Hello Prep1 World!");
+        // int x;
+        // x = 5;
+        // string y;
+        // y = "number is: ";
+        // Console.WriteLine(y + x);
+        // Console.WriteLine(x);
+
+        // Console.Write("What is your favorite color? ");
+        // string color = Console.ReadLine();
+        // Console.WriteLine("Favorite color is " + color);
+        // Console.WriteLine($"My favorite color is {color}");
+
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+        Console.WriteLine("");
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
+
     }
 }   
